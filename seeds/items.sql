18cde0a1e667defdab2d1d39eaf66e331f5c2958
AdminAlex <papagiannis.a.999@gmail.com>
2024-03-11 14:20:03 +0200

    Implemented the seeds files for creating and populating the items and orders tables

---
+++ seeds/items.sql
@@ -0,0 +1,9 @@
+TRUNCATE TABLE items RESTART IDENTITY; -- replace with your own table name.
+
+-- Below this line there should only be `INSERT` statements.
+-- Replace these statements with your own seed data.
+
+INSERT INTO items (name, price, quantity) VALUES ('First item', 4.5, 23);
+INSERT INTO items (name, price, quantity) VALUES ('Second item', 7.0, 31);
+INSERT INTO items (name, price, quantity) VALUES ('Third item', 12.9, 11);
+INSERT INTO items (name, price, quantity) VALUES ('Fourth item', 29.35, 5);
---
+++ seeds/orders.sql
@@ -0,0 +1,11 @@
+TRUNCATE TABLE orders RESTART IDENTITY; -- replace with your own table name.
+
+-- Below this line there should only be `INSERT` statements.
+-- Replace these statements with your own seed data.
+
+INSERT INTO orders (customer_name, date, item_id) VALUES ('First customer', '2024-03-02', 2);
+INSERT INTO orders (customer_name, date, item_id) VALUES ('Second customer', '2024-03-04', 4);
+INSERT INTO orders (customer_name, date, item_id) VALUES ('Third customer', '2024-03-05', 1);
+INSERT INTO orders (customer_name, date, item_id) VALUES ('Fourth customer', '2024-03-07', 3);
+INSERT INTO orders (customer_name, date, item_id) VALUES ('Fifth customer', '2024-03-08', 1);
+INSERT INTO orders (customer_name, date, item_id) VALUES ('Sixth customer', '2024-03-11', 2);
